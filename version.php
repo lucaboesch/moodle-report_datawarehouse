b4ebb32ee3bdd78e816144998fa9ea1a61fc2189
Erik <erik.steinacher@gmail.com>
2026-04-02 22:24:10 +0200

    feat: Add CSV download option, external file retrieval API, and CSV generation enhancements

--- version.php
+++ version.php
@@ -25,9 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'report_datawarehouse';
-$plugin->release = '0.9.2';
 $plugin->version = 2026031800;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [402, 501];
-$plugin->release   = 'v5.1-r1';
+$plugin->release   = 'v5.1-r2';
